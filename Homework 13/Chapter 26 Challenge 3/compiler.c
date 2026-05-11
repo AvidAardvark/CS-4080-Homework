29296e004c5885fa118e906a5d2042348006ac87
AvidAardvark <144076718+AvidAardvark@users.noreply.github.com>
2026-05-10 19:20:16 -0700

    Create compiler.c

---
+++ Homework 13/Chapter 26 Challenge 3/compiler.c
@@ -0,0 +1,12 @@
+static void initCompiler(Compiler* compiler, FunctionType type) {
+  compiler->function = newFunction();
+  // This function is referenced by the compiler
+  incRef((Obj*) compiler->function);
+
+static ObjFunction* endCompiler() {
+  current = current->enclosing;
+  // Function no longer referenced by the compiler
+  decRef((Obj*)function);
+  return function;
+}
+
