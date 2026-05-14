3d2611f6c9687e63394ed8cb6918f65a3fcf8707
AvidAardvark <144076718+AvidAardvark@users.noreply.github.com>
2026-05-13 21:55:50 -0700

    Update object.h

--- Homework 14/Chapter 28 Challenge 1/object.h
+++ Homework 14/Chapter 28 Challenge 1/object.h
@@ -5,11 +5,4 @@ typedef struct ObjClass {
   Table methods;
 } ObjClass;
 
-ObjClass* newClass(ObjString* name) {
-  ObjClass* klass = ALLOCATE_OBJ(ObjClass, OBJ_CLASS);
-  klass->name = name;
-  klass->initializer = NIL_VAL; 
-  initTable(&klass->methods);
-  return klass;
-}
 
